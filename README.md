aa2fad43d77d3589f1b038c5a99b9e5bdfd3c986
dsci <dsci@code79.net>
2020-03-11 10:25:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngneat-transloco-hvqvsm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngneat-transloco-hvqvsm)
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { httpLoader } from "./http-loader";
+
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import {
+  TRANSLOCO_CONFIG,
+  TranslocoConfig,
+  TranslocoModule
+} from "@ngneat/transloco";
+import { TranslocoMessageFormatModule } from '@ngneat/transloco-messageformat';
+import { HomeComponent } from "./home/home.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+@NgModule({
+  imports: [
+    BrowserModule, 
+    TranslocoModule, 
+    AppRoutingModule, 
+    HttpClientModule,
+    TranslocoMessageFormatModule.init()],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  providers: [
+    httpLoader,
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: {
+        availableLangs: ["en", "es"],
+        reRenderOnLangChange: true,
+        fallbackLang: "es",
+        defaultLang: "en"
+      } as TranslocoConfig
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
